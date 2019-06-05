8cc03725570f5ebedc841df0ad075aa04cc7c5ef
Andrew Kim <akinhwan@gmail.com>
2019-06-05 17:57:51 -0400

    html with bulma

--- betalist.py
+++ betalist.py
@@ -1,7 +1,12 @@
 from textgenrnn import textgenrnn
-textgen = textgenrnn(weights_path='betalist_model_weights.hdf5',
-                       vocab_path='betalist_model_vocab.json',
-                       config_path='betalist_model_config.json')
 
-textgen.generate_samples(max_gen_length=1000)
-textgen.generate_to_file('textgenrnn_texts.txt', max_gen_length=1000)
+def main(event, context):
+    textgen = textgenrnn(weights_path='betalist_model_weights.hdf5',
+                        vocab_path='betalist_model_vocab.json',
+                        config_path='betalist_model_config.json')
+
+    textgen.generate_samples(max_gen_length=1000)
+    textgen.generate_to_file('betalist_output.txt', max_gen_length=1000)
+
+if __name__ == "__main__":
+    main('','')
